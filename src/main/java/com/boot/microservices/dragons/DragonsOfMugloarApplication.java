d39ae382c399a6802171f3be655933cc1c644791
Imre Purret <imre.purret@gmail.com>
2025-01-14 18:34:53 +0200

    Initial Spring Boot application

---
+++ src/main/java/com/boot/microservices/dragons/DragonsOfMugloarApplication.java
@@ -0,0 +1,13 @@
+package com.boot.microservices.dragons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonsofMugloarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonsofMugloarApplication.class, args);
+	}
+
+}
