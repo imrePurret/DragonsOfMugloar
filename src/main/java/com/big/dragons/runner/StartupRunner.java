2ebfe7bc4e7126aa8ea2e8274fcfaf4e0751d6c5
Imre Purret <imre.purret@gmail.com>
2025-01-24 03:19:09 +0200

    Small cleaning and fixes

--- src/main/java/com/big/dragons/runner/StartupRunner.java
+++ src/main/java/com/big/dragons/runner/StartupRunner.java
@@ -7,9 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("run")
 @RequiredArgsConstructor
 public class StartupRunner implements CommandLineRunner {
 
