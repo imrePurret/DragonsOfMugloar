e20c6984529dad1051536a6052f6293a88ed7fb3
Imre Purret <imre.purret@gmail.com>
2025-01-24 03:50:45 +0200

    Fixing issue with StartupRunner activating while tests are running

--- src/main/java/com/big/dragons/runner/StartupRunner.java
+++ src/main/java/com/big/dragons/runner/StartupRunner.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("run")
+@Profile("!test")
 @RequiredArgsConstructor
 public class StartupRunner implements CommandLineRunner {
 
